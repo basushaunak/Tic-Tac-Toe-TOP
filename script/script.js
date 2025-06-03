4d1cd3d9f0ace0da38626db7efbecd2dfa4dc7d5
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-06-03 16:52:54 +0530

    WIP

--- script/script.js
+++ script/script.js
@@ -1,1 +1,22 @@
 // JavaScript file for Tic-Tac-Toe
+const gameGrid = document.querySelector("#game-grid");
+const player1 = {};
+const player2 = {};
+
+const Player = function (name,marker) {
+    return {name,marker};
+}
+
+
+function initGame(){
+    player1 = new Player(document.querySelector("#player1","X"));
+    player2 = new Player(document.querySelector("#player2","O"));
+}
+
+document.querySelector("btn-start-game".addEventListener("click",(e)=>{
+    initGame();
+}))
+
+gameGrid.addEventListener("click",(e)=>{
+    markGrid(e.target,player);
+})
