bac1661b1264d3a7b8f437b8d4345ebb72ee196e
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-06-06 11:44:02 +0530

    WIP

--- script/script.js
+++ script/script.js
@@ -1,22 +1,44 @@
 // JavaScript file for Tic-Tac-Toe
-const gameGrid = document.querySelector("#game-grid");
-const player1 = {};
-const player2 = {};
-
-const Player = function (name,marker) {
-    return {name,marker};
-}
 
 
-function initGame(){
-    player1 = new Player(document.querySelector("#player1","X"));
-    player2 = new Player(document.querySelector("#player2","O"));
-}
+function playGame(){  
+  let activePlayer = 0;
+  let winner = -1;
+  let players = [];
+  const playerMovePrompt = document.querySelector("#player-move-prompt");
+  const gameGrid = document.querySelector("#game-grid");
+  players.push(createPlayer(document.querySelector("#player1").value,"X"));
+  players.push(createPlayer(document.querySelector("#player2").value,"O"));
+  document.querySelector("#game-in-progress").style.display = "block";
+  showPrompt("Waiting for", players[activePlayer]);
+    
+  gameGrid.addEventListener("click",(e)=>{
+    markGrid(e.target,players[activePlayer].marker);
+  })
 
-document.querySelector("btn-start-game".addEventListener("click",(e)=>{
-    initGame();
-}))
+  function markGrid(cell,marker){
+    if(cell.innerText === ""){
+      cell.innerText = marker;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      showPrompt("Waiting for", players[activePlayer]);
+      checkWinner();
+    }
+  }
+  function checkWinner(){
+    // returns -2 if Draw, -1 if still to be continued, 0 if plaaaaayer 1 won, 1 if player 2 won
+  }
+  function createPlayer(playerName, marker){
+    return {playerName, marker};
+  }
+  function showPrompt(message, player){
+    playerMovePrompt.innerText = `${message} ${player.playerName}`;
+  }
+}    
 
-gameGrid.addEventListener("click",(e)=>{
-    markGrid(e.target,player);
+document.querySelector("#btn-start-game").addEventListener("click",(e)=>{
+    playGame();
 })
+
+window.onload = ()=>{
+  document.querySelector("#game-in-progress").style.display = "none";
+}
